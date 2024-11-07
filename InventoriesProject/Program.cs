0d189f93081efc36fef012c145b5b19b4b1fbdb5
aidanur9 <aidanurazizah9@gmail.com>
2024-11-07 07:26:04 +0700

    feat: created web project using Web API template

---
+++ InventoriesProject/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using InventoriesProject.Data;
+using InventoriesProject.Repositories;
+using InventoriesProject.Services;
+
+namespace InventoriesProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configs = builder.Configuration;
+
+/*            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configs.GetConnectionString("DefaultConnection"));
+            });
+
+            // registers repositories
+            builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
+
+            // registers services
+            builder.Services.AddScoped<ISupplierService, SupplierService>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<IInventoryService, InventoryService>();
+*/
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
